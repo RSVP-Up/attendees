d11b3eaebc6ef560af2f25436c9ac0d6c1d383b2
Emily Kwak <emilykwak1@gmail.com>
2019-12-30 17:55:03 -0800

    render Attendees component

---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Attendees from './components/Attendees.jsx';
+
+ReactDOM.render(<Attendees />, document.getElementById('attendees'));

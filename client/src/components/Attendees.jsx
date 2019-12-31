53759f45561395c3c895d16e8e05b76aac986338
Emily Kwak <emilykwak1@gmail.com>
2019-12-30 18:58:47 -0800

    fetch event attendees and waitlist

--- client/src/components/Attendees.jsx
+++ client/src/components/Attendees.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import axios from 'axios';
+import queryString from 'query-string';
 
 class Attendees extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      attendees: [],
+      waitlist: []
+    }
+  }
+
+  componentDidMount() {
+    const value = queryString.parse(window.location.search)
+    const id = value.event_id
+
+    axios.get(`http://127.0.0.1:3001/rsvp/${id}`)
+      .then((members) => {
+        this.setState({
+          attendees: members.data.attendees,
+          waitlist: members.data.waitlist
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+
   render() {
+    console.log(this.state)
     return (
       <div>
         <h1>Attendees</h1>
